df14b89644afcc53f6222d72b5398742969b5204
buchiyu <lbuchiyu@gmail.com>
2022-08-20 17:24:11 +0800

    :art: prettier格式化代码

--- plugins/everyday/src/index.ts
+++ plugins/everyday/src/index.ts
@@ -1,39 +1,39 @@
-import { Context, Schema, Session, Command, segment } from 'koishi'
-import '@koishijs/plugin-adapter-telegram'
-import '@koishijs/plugin-adapter-onebot'
+import { Context, Schema, Session, Command, segment } from 'koishi';
+import '@koishijs/plugin-adapter-telegram';
+import '@koishijs/plugin-adapter-onebot';
 
-export const name = 'everyday'
+export const name = 'everyday';
 
-const guildList = ['290061546', '795802565', '702547164', '726356752']
+const guildList = ['290061546', '795802565', '702547164', '726356752'];
 
 export function apply(ctx: Context) {
   // 处理加群请求
   ctx.on('guild-member-request', async (session) => {
-    const { messageId, guildId } = session
-    const { max_member_count, member_count } = await session.onebot.getGroupInfo(guildId)
+    const { messageId, guildId } = session;
+    const { max_member_count, member_count } = await session.onebot.getGroupInfo(guildId);
     if (max_member_count > member_count) {
-      await session.bot.handleGuildMemberRequest(messageId, true)
+      await session.bot.handleGuildMemberRequest(messageId, true);
     } else {
-      let confuseText = '该群已满!'
-      let index = 1
+      let confuseText = '该群已满!';
+      let index = 1;
       for await (const gId of guildList) {
-        const { max_member_count, member_count } = await session.onebot.getGroupInfo(gId)
-        confuseText += `${index++}群：${gId}(${member_count}/${max_member_count})；`
+        const { max_member_count, member_count } = await session.onebot.getGroupInfo(gId);
+        confuseText += `${index++}群：${gId}(${member_count}/${max_member_count})；`;
       }
-      await session.bot.handleGuildMemberRequest(messageId, false, confuseText)
+      await session.bot.handleGuildMemberRequest(messageId, false, confuseText);
     }
-  })
+  });
   // 处理踢人请求
   ctx.on('message', async (session) => {
-    const { messageId, content, guildId, userId } = session
+    const { messageId, content, guildId, userId } = session;
     if (guildList.includes(guildId) && content === '【我想退群了】') {
       // await session.onebot.deleteMsg(messageId)
-      await session.onebot.setGroupKick(guildId, userId)
+      await session.onebot.setGroupKick(guildId, userId);
     }
-  })
+  });
 
-
-  ctx.command('go <message:text>', { authority: 2 })
+  ctx
+    .command('go <message:text>', { authority: 2 })
     .option('anonymous', '-a', { authority: 3 })
     .option('forceAnonymous', '-A', { authority: 3 })
     .option('escape', '-e', { authority: 3 })
@@ -42,7 +42,7 @@ export function apply(ctx: Context) {
     .option('channel', '-c [channel:channel]', { authority: 3 })
     .option('guild', '-g [guild:string]', { authority: 3 })
     .action(async ({ options, session }, message) => {
-      if (!message) return "请输入消息！"
-      return 'gkd'
-    })
+      if (!message) return '请输入消息！';
+      return 'gkd';
+    });
 }
--- plugins/shop/src/index.ts
+++ plugins/shop/src/index.ts
@@ -1,91 +1,87 @@
-import {Command, Context, Schema, Session} from 'koishi'
+import { Command, Context, Schema, Session } from 'koishi';
 
 declare module 'koishi' {
   interface Channel {
-    shopKeywords: string[]
+    shopKeywords: string[];
   }
 }
 
 export interface Config {
-  discountGroup?: string[]
-  master?: string
+  discountGroup?: string[];
+  master?: string;
 }
 
 export const Config: Schema<Config> = Schema.object({
   discountGroup: Schema.array(String).default([]).description('要监测的群'),
   master: Schema.string().description('监控信息的转发人'),
-})
+});
 
-export const name = 'shop'
+export const name = 'shop';
 
-export function apply(ctx: Context,{ discountGroup, master }: Config) {
+export function apply(ctx: Context, { discountGroup, master }: Config) {
   ctx.before('attach-channel', (session, fields) => {
-    fields.add('shopKeywords')
-  })
+    fields.add('shopKeywords');
+  });
 
   ctx.middleware((_: Session<never, 'shopKeywords'>, next) => {
-    const { channel, bot } = _
-    const { shopKeywords } = channel || {}
-    ctx.app.guild(...discountGroup).on("message", async (session) => {
+    const { channel, bot } = _;
+    const { shopKeywords } = channel || {};
+    ctx.app.guild(...discountGroup).on('message', async (session) => {
       for await (const keyword of shopKeywords) {
         if (session.content.includes(keyword)) {
           await bot.sendPrivateMessage(
             master,
-            `${
-              new Date(session.timestamp).toLocaleString("zh", { timeZone: "Asia/shanghai", hour12: false })
-            }\n${session.content}`
-          )
+            `${new Date(session.timestamp).toLocaleString('zh', { timeZone: 'Asia/shanghai', hour12: false })}\n${
+              session.content
+            }`,
+          );
         }
       }
-    })
+    });
 
-    return next()
-  })
+    return next();
+  });
 
   ctx.model.extend('channel', {
     shopKeywords: 'list',
-  })
+  });
 
   ctx.using(['database'], (ctx) => {
-    const cmd = ctx
-      .command('shop [operation:string] <keyword:keyword>')
+    const cmd = ctx.command('shop [operation:string] <keyword:keyword>');
 
-    const register = (def: string, callback: Command.Action<never, "shopKeywords", [string]>) => cmd
-      .subcommand(def, { checkArgCount: true })
-      .channelFields(["shopKeywords"])
-      .action(callback)
+    const register = (def: string, callback: Command.Action<never, 'shopKeywords', [string]>) =>
+      cmd.subcommand(def, { checkArgCount: true }).channelFields(['shopKeywords']).action(callback);
 
     register('.add <keyword:keyword>', async ({ session }, keyword) => {
-      const { shopKeywords } = session.channel
+      const { shopKeywords } = session.channel;
       if (shopKeywords.includes(keyword)) {
-        return session.text(`${keyword} 已经是关键词啦！`)
+        return session.text(`${keyword} 已经是关键词啦！`);
       } else {
-        shopKeywords.push(keyword)
-        return session.text(`已成功添加关键词： ${keyword}！`)
+        shopKeywords.push(keyword);
+        return session.text(`已成功添加关键词： ${keyword}！`);
       }
-    }).alias('shop.add')
+    }).alias('shop.add');
 
     register('.remove <keyword:keyword>', async ({ session }, keyword) => {
-      const { shopKeywords } = session.channel
-      const index = shopKeywords.indexOf(keyword)
+      const { shopKeywords } = session.channel;
+      const index = shopKeywords.indexOf(keyword);
       if (index >= 0) {
-        shopKeywords.splice(index, 1)
-        return session.text(`已成功移除关键词： ${keyword}!`)
+        shopKeywords.splice(index, 1);
+        return session.text(`已成功移除关键词： ${keyword}!`);
       } else {
-        return session.text(`${keyword} 不是关键词!`)
+        return session.text(`${keyword} 不是关键词!`);
       }
-    }).alias('shop.rm')
+    }).alias('shop.rm');
 
     register('.clear', async ({ session }) => {
-      session.channel.shopKeywords = []
-      return session.text('已成功移除全部关键词!')
-    }).alias('shop.clear')
+      session.channel.shopKeywords = [];
+      return session.text('已成功移除全部关键词!');
+    }).alias('shop.clear');
 
     register('.list', async ({ session }) => {
-      const { shopKeywords } = session.channel
-      if (!shopKeywords.length) return session.text('暂无关键词！')
-      return [session.text('关键词列表：'), ...shopKeywords].join('\n')
-    }).alias('shop.ls')
-
-  })
+      const { shopKeywords } = session.channel;
+      if (!shopKeywords.length) return session.text('暂无关键词！');
+      return [session.text('关键词列表：'), ...shopKeywords].join('\n');
+    }).alias('shop.ls');
+  });
 }
