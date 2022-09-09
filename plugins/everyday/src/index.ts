57c9d66a177ca0a8db1d2fd857f294685cf02dcf
buchiyu <lbuchiyu@gmail.com>
2022-09-10 00:12:36 +0800

    feat: add 一键移除重复加群成功

--- plugins/everyday/src/index.ts
+++ plugins/everyday/src/index.ts
@@ -1,10 +1,12 @@
 import { Context, Schema, Session, Command, segment } from 'koishi';
+import _ from 'lodash';
 import '@koishijs/plugin-adapter-telegram';
 import '@koishijs/plugin-adapter-onebot';
 
 export const name = 'everyday';
 
-const guildList = ['290061546', '795802565', '702547164', '726356752'];
+const guildList = [290061546, 795802565, 702547164, 726356752];
+const masterGuildList = ['#', '532250819'];
 
 export function apply(ctx: Context) {
   // 处理加群请求
@@ -26,23 +28,41 @@ export function apply(ctx: Context) {
   // 处理踢人请求
   ctx.on('message', async (session) => {
     const { messageId, content, guildId, userId } = session;
-    if (guildList.includes(guildId) && content === '【我想退群了】') {
+    if (guildList.includes(Number(guildId)) && content === '【我想退群了】') {
       // await session.onebot.deleteMsg(messageId)
       await session.onebot.setGroupKick(guildId, userId);
     }
   });
+  // 查看一人多群
+  ctx.on('message', async (session) => {
+    const { messageId, content, guildId, userId } = session;
+    if (masterGuildList.includes(guildId) && content === '【一人多群】') {
+      for (const [index, guild] of guildList.entries()) {
+        const memberListTasks = guildList.slice(index).map((g) => {
+          return session.onebot?.getGroupMemberList(g);
+        });
+        const [memberList, ...otherMemberList] = await Promise.all(memberListTasks);
+        for (const otherMember of otherMemberList) {
+          const intersection = _.intersectionBy(otherMember, memberList, 'user_id').filter(
+            (item) => item?.role === 'member',
+          );
 
-  ctx
-    .command('go <message:text>', { authority: 2 })
-    .option('anonymous', '-a', { authority: 3 })
-    .option('forceAnonymous', '-A', { authority: 3 })
-    .option('escape', '-e', { authority: 3 })
-    .option('unescape', '-E', { authority: 3 })
-    .option('user', '-u [user:user]', { authority: 3 })
-    .option('channel', '-c [channel:channel]', { authority: 3 })
-    .option('guild', '-g [guild:string]', { authority: 3 })
-    .action(async ({ options, session }, message) => {
-      if (!message) return '请输入消息！';
-      return 'gkd';
-    });
+          const privateMsgTasks = intersection.map((item) => {
+            const { group_id, user_id } = item;
+            return session.onebot?.sendPrivateMsg(
+              user_id,
+              `打扰了！您额外加入了脏果君资源群(${group_id})，已将您移除，谢谢配合`,
+            );
+          });
+          await Promise.all([].concat(privateMsgTasks, privateMsgTasks));
+
+          const delMemberTasks = intersection.map((item) => {
+            const { group_id, user_id } = item;
+            return session.onebot?.setGroupKick(group_id, user_id);
+          });
+          await Promise.all([].concat(delMemberTasks, privateMsgTasks));
+        }
+      }
+    }
+  });
 }
